f7a5a4b4e709166f0295277f8190299358897ac4
monwolf <berni.emerald@gmail.com>
2025-12-21 09:18:39 +0100

    refactor: simplify Kafka scripts by removing version checks and using bootstrap server directly

--- scripts/addPartitions.sh
+++ scripts/addPartitions.sh
@@ -9,25 +9,6 @@ find_container_id() {
     -q)
 }
 
-get_kafka_version() {
-  local container_id=$(find_container_id)
-  local version_output=$(docker exec $container_id bash -c "kafka-broker-api-versions --version 2>&1 | grep -oP '(\d+)\.(\d+)\.(\d+)' | head -1" || echo "2.5.0")
-  echo $version_output
-}
-
-should_use_bootstrap_server() {
-  local version=$1
-  local major=$(echo $version | cut -d. -f1)
-  local minor=$(echo $version | cut -d. -f2)
-  
-  # Kafka 2.5+ should use --bootstrap-server
-  if [ $major -gt 2 ] || ([ $major -eq 2 ] && [ $minor -ge 5 ]); then
-    echo "true"
-  else
-    echo "false"
-  fi
-}
-
 if [ -z "$TOPIC" ]; then
   echo -d "TOPIC is mandatory"
   exit 1
@@ -38,15 +19,6 @@ if [ -z "$PARTITIONS" ]; then
   exit 1
 fi
 
-KAFKA_VERSION=$(get_kafka_version)
-USE_BOOTSTRAP=$(should_use_bootstrap_server $KAFKA_VERSION)
-
-if [ "$USE_BOOTSTRAP" = "true" ]; then
-  CONNECTION_PARAM="--bootstrap-server kafka1:9092"
-else
-  CONNECTION_PARAM="--zookeeper zookeeper:2181"
-fi
-
 docker exec \
   $(find_container_id) \
-  bash -c "kafka-topics ${CONNECTION_PARAM} --alter --topic ${TOPIC} --partitions ${PARTITIONS}"
+  bash -c "kafka-topics --bootstrap-server kafka1:9092 --alter --topic ${TOPIC} --partitions ${PARTITIONS}"
